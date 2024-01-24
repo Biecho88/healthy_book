7bd3aabba1ae799b2083fb3f9fd55b3407670384
Biecho88 <>
2024-01-24 22:52:31 +0000

    Connect Flask to MongoDb

--- run.py
+++ run.py
@@ -2,17 +2,29 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello world"
+@app.route("/get_category")
+def get_category():
+    category = mongo.db.category.find()
+    return render_template("category.html", category=category)
 
 
 if __name__ == "__main__":
